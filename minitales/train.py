f0ffc5778e02de2c54262140167a9e5d025b9f41
= <normoslem256@gmail.com>
2024-12-15 13:28:32 +0300

    Refactor training script in train.py to enhance functionality
    - Updated the train function to accept customizable epochs and sub-epochs parameters.
    - Improved import statements for the SimpleTokenizer class.
    - Added checks for vocabulary file existence and integrated tokenizer training if the file is missing.
    - Streamlined dataset and tokenizer loading processes for better clarity and maintainability.
    - Included print statements for better visibility during execution.

--- minitales/train.py
+++ minitales/train.py
@@ -1,15 +1,12 @@
 import torch
 
-def train(model, optimizer, criterion, tokens, block_size, batch_size, device):
+def train(model, optimizer, criterion, tokens, block_size, batch_size, device, epochs=20, sub_epochs=1000):
     """
     Train the model using the given optimizer and criterion.
     """
 
     upper = len(tokens) - block_size - 1
-
-    epochs = 20
-    sub_epochs = 1000
-
+    
     print(f'Training for {epochs} epochs with {sub_epochs} sub-epochs each')
     for epoch in range(epochs):
         model.train()  # Set the model to training mode
@@ -35,8 +32,16 @@ def train(model, optimizer, criterion, tokens, block_size, batch_size, device):
 
 def main():
     from dataset import load_dataset
-    from tokenizer import SimpleTokenizer
+    from tokenizer.tokenizer import SimpleTokenizer
     from model import Transformer
+    import os
+
+    ds_path = 'dataset/TinyStories10k_eos.txt'
+    vocab_path = 'tokenizer/vocab10k.json'
+
+    if not os.path.exists(vocab_path):
+        from tokenizer.train import main as tokenizer_train
+        tokenizer_train(vocab_path)
 
     from torch import optim, nn
 
@@ -44,9 +49,9 @@ def main():
     print(f'Using device: {device}')
 
     print('Loading dataset...')
-    ds = load_dataset('dataset/TinyStories10k_eos.txt')
-    print('Tokenizing...')
-    tokenizer = SimpleTokenizer('vocab/vocab10k.json', special_tokens=['<|eos|>'])
+    ds = load_dataset(ds_path)
+    print('Loading tokenizer...')
+    tokenizer = SimpleTokenizer(vocab_path, special_tokens=['<|eos|>'])
     print('Encoding...')
     tokens = tokenizer.encode(ds)
 
@@ -57,13 +62,16 @@ def main():
     batch_size = 32
 
     # Corrected class name (assuming typo)
+    print('Loading model...')
     model = Transformer(vocab_size, n_embedding, block_size, n_heads).to(device)
+    print('Loading tokens as tensor...')
     tensor_tokens = torch.tensor(tokens, dtype=torch.long, device=device)
 
     # Loss and Optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
+    print('Training...')
     train(model, optimizer, criterion, tensor_tokens, block_size, batch_size, device)
 
 if __name__ == '__main__':
