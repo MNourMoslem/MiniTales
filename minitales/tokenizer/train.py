d923e51fde80962d9cb8c4f166e2eca79226b503
= <normoslem256@gmail.com>
2024-12-15 13:52:20 +0300

    Refactor tokenizer training script in train.py to improve structure and flexibility
    - Removed the main function and unnecessary import statements for a cleaner codebase.
    - Introduced a new train_tokenizer function that accepts dataset and training parameters, enhancing usability.
    - Maintained functionality for training the SimpleTokenizer and saving the vocabulary.
    - Improved code readability and maintainability by streamlining the function's parameters.

--- minitales/tokenizer/train.py
+++ minitales/tokenizer/train.py
@@ -1,12 +1,6 @@
-from dataset import load_dataset
-from tokenizer.tokenizer import SimpleTokenizer
+from minitales.tokenizer.tokenizer import SimpleTokenizer
 
-def main(vocab_path):
-    ds = load_dataset('dataset/TinyStories10k_eos.txt')
-    tokenizer = SimpleTokenizer(special_tokens=["<|eos|>"])
-    tokenizer.train(ds, num_tokens=10000, k=100, add_remaining_tokens=True, detailed_print=True)
-
+def train_tokenizer(vocab_path, ds, num_tokens=10000, k=100, special_tokens=["<|eos|>"], **kwargs):
+    tokenizer = SimpleTokenizer(special_tokens=special_tokens)
+    tokenizer.train(ds, num_tokens=num_tokens, k=k, add_remaining_tokens=True, detailed_print=True, **kwargs)
     tokenizer.save(vocab_path)
-
-if __name__ == '__main__':
-    main('tokenizer/vocab10k.json')
