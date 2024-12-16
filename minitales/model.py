fb5bc55e4e7495c1b1cdb88f7fc384397eba140d
= <normoslem256@gmail.com>
2024-12-16 12:59:31 +0300

    Refactor model architecture in model.py
    - Renamed and reorganized classes for clarity: 'FeedForward' to 'FeedFoward', 'MultiHeadAttention' and 'Head' classes were introduced to improve modularity.
    - Updated the forward methods to enhance attention mechanism and dropout handling.
    - Improved initialization of weights for better training stability.
    - Streamlined the attention calculation process and added support for masked attention.
    - Enhanced the overall structure of the model to facilitate future extensions and improvements.

--- minitales/model.py
+++ minitales/model.py
@@ -1,109 +1,122 @@
 import torch
-import torch.nn as nn
+from torch import nn
 import torch.nn.functional as F
 
-class FeedForward(nn.Module):
-  def __init__(self, n_embedding):
+class Head(nn.Module):
+  def __init__(self, embed_size, head_size, max_len, dropout):
     super().__init__()
 
-    self.net = nn.Sequential(
-        nn.Linear(n_embedding, 512),
-        nn.ReLU(),
-        nn.Linear(512, 1024),
-        nn.ReLU(),
-        nn.Linear(1024, 512),
-        nn.ReLU(),
-        nn.Linear(512, n_embedding)
-    )
+    self.head_size = head_size
 
-  def forward(self, x):
-    return self.net(x)
+    self.Q = nn.Linear(embed_size, head_size)
+    self.K = nn.Linear(embed_size, head_size)
+    self.V = nn.Linear(embed_size, head_size)
+
+    self.register_buffer('tril', torch.tril(torch.ones(max_len, max_len)))
+
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, x, masked = False):
+    B, T, E = x.shape # B, T, E
+
+    q = self.Q(x) # B, T, H
+    k = self.K(x) # B, T, H
+
+    w = q @ k.transpose(-2, -1) * self.head_size ** -0.5 # B, T, H @ B, H, T -> B, T, T
+    if masked and self.training:
+      w = w.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # B, T, T
+    w = F.softmax(w, -1) # B, T, T
+    w = self.dropout(w) # B, T, T
+
+    v = self.V(x) # B, T, H
+    out = w @ v # B, T, T @ # B, T, H -> # B, T, H
+
+    return out # B, T, H
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, block_size, n_embedding, n_heads, masked=False):
-        super().__init__()
-
-        self.n_embedding = n_embedding
-        self.n_heads = n_heads
-        self.head_dim = n_embedding // n_heads
-        assert self.head_dim * n_heads == n_embedding, "Embedding dimension must be divisible by the number of heads"
-
-        self.masked = masked
-
-        self.Q = nn.Linear(n_embedding, n_embedding, bias=False)
-        self.K = nn.Linear(n_embedding, n_embedding, bias=False)
-        self.V = nn.Linear(n_embedding, n_embedding, bias=False)
-
-        self.fc_out = nn.Linear(n_embedding, n_embedding)
-
-        if masked:
-            self.register_buffer('tril', torch.tril(torch.ones((block_size, block_size))))
-
-    def forward(self, x):
-        batch_size, seq_length, _ = x.shape
-
-        q = self.Q(x)  # (B, L, E)
-        k = self.K(x)  # (B, L, E)
-        v = self.V(x)  # (B, L, E)
-
-        # Reshape into multiple heads
-        q = q.view(batch_size, seq_length, self.n_heads, self.head_dim).transpose(1, 2) # (B, N, L, H)
-        k = k.view(batch_size, seq_length, self.n_heads, self.head_dim).transpose(1, 2) # (B, N, L, H)
-        v = v.view(batch_size, seq_length, self.n_heads, self.head_dim).transpose(1, 2) # (B, N, L, H)
-
-        # Attention calculation
-        wei = q @ k.transpose(-2, -1) / self.head_dim ** 0.5 # (B, N, L, L)
-        if self.masked and self.training:
-          wei = wei.masked_fill(self.tril[:seq_length, :seq_length] == 0, float('-inf'))
-
-        attention = F.softmax(wei, dim=-1) #(B, N, L)
-
-        out = attention @ v  # (batch_size, n_heads, seq_length, head_dim)
-
-        # Concatenate heads and pass through final linear layer
-        out = out.transpose(1, 2).contiguous().view(batch_size, seq_length, self.n_embedding)
-        out = self.fc_out(out)
-
-        return out
-
-class Decoder(nn.Module):
-    def __init__(self, vocab_size, n_embedding, block_size, n_heads):
-        super().__init__()
-
-        self.token_embedding = nn.Embedding(vocab_size, n_embedding)
-        self.pos_embedding = nn.Embedding(block_size, n_embedding)
-
-        self.masked_head = MultiHeadAttention(block_size, n_embedding, n_heads, masked=True)
-        self.head = MultiHeadAttention(block_size, n_embedding, n_heads, masked=False)
-        self.feedforward = FeedForward(n_embedding)
-
-        self.layernorm = nn.LayerNorm(n_embedding)
-
-    def forward(self, x):
-        token_embeds = self.token_embedding(x)
-        pos_embeds = self.pos_embedding(torch.arange(x.size(1), device=x.device))
-
-        embeds = token_embeds + pos_embeds
-
-        masked_res = self.masked_head(embeds)
-        add_norm_res = self.layernorm(embeds + masked_res)
-
-        head_res = self.head(add_norm_res)
-        add_norm_res = self.layernorm(add_norm_res + head_res)
-
-        feed_res = self.feedforward(add_norm_res)
-        add_norm_res = self.layernorm(add_norm_res + feed_res)
-
-        return add_norm_res
-
-class Transformer(nn.Module):
-
-  def __init__(self, vocab_size, n_embedding, block_size, n_heads):
+  def __init__(self, embed_size, n_heads, max_len, masked, dropout = 0.2):
     super().__init__()
+    
+    head_size = embed_size // n_heads
 
-    self.decoder = Decoder(vocab_size, n_embedding, block_size, n_heads)
-    self.output = nn.Linear(n_embedding, vocab_size)
+    assert head_size * n_heads == embed_size , "n_heads must be truly divisible on embed_size"
+
+    self.heads = nn.ModuleList([Head(embed_size, head_size, max_len, dropout) for _ in range(n_heads)])
+    self.proj = nn.Linear(head_size * n_heads, embed_size)
+    self.dropout = nn.Dropout(dropout)
+
+    self.masked = masked
 
   def forward(self, x):
-    x = self.decoder(x)
-    return self.output(x)
+    out = torch.cat([h(x, self.masked) for h in self.heads], dim = -1)
+    out = self.dropout(self.proj(x))
+    return out
+
+
+class FeedFoward(nn.Module):
+  def __init__(self, embed_size, dropout = 0.2):
+    super().__init__()
+    
+    self.net = nn.Sequential(
+        nn.Linear(embed_size, 4 * embed_size),
+        nn.ReLU(),
+        nn.Linear(4 * embed_size, 6 * embed_size),
+        nn.ReLU(),
+        nn.Linear(6 * embed_size, 4 * embed_size),
+        nn.ReLU(),
+        nn.Linear(4 * embed_size, embed_size),
+    )
+
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, x):
+    x = self.net(x)
+    x = self.dropout(x)
+    return x
+
+class Block(nn.Module):
+  def __init__(self, embed_size, n_heads, max_len, dropout):
+    super().__init__()
+    self.sa = MultiHeadAttention(embed_size, n_heads, max_len, True, 0.2)
+    self.ffwd = FeedFoward(embed_size, dropout)
+    self.ln1 = nn.LayerNorm(embed_size)
+    self.ln2 = nn.LayerNorm(embed_size)
+
+  def forward(self, x):
+      x = x + self.sa(self.ln1(x))
+      x = x + self.ffwd(self.ln2(x))
+      return x
+
+
+class MT(nn.Module):
+  def __init__(self, embed_size, vocab_size, n_layers, n_heads, max_len, dropout):
+    super().__init__()
+
+    self.token_embedding_table = nn.Embedding(vocab_size, embed_size)
+    self.position_embedding_table = nn.Embedding(max_len, embed_size)
+
+    self.blocks = nn.Sequential(*[Block(embed_size, n_heads, max_len, dropout) for _ in range(n_layers)])
+    self.ln = nn.LayerNorm(embed_size)
+    self.lm_head = nn.Linear(embed_size, vocab_size)
+
+    self.apply(self._init_weights)
+
+  def _init_weights(self, module):
+      if isinstance(module, nn.Linear):
+          torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+          if module.bias is not None:
+              torch.nn.init.zeros_(module.bias)
+      elif isinstance(module, nn.Embedding):
+          torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+  def forward(self, idx, targets=None):
+      B, T = idx.shape
+
+      tok_emb = self.token_embedding_table(idx) # (B,T,C)
+      pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+      x = tok_emb + pos_emb # (B,T,C)
+      x = self.blocks(x) # (B,T,C)
+      x = self.ln(x) # (B,T,C)
+      logits = self.lm_head(x) # (B,T,vocab_size)
+
+      return logits
