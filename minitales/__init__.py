b4f27c2fc42e24ef70a2c9d30ed6610f92e3c5b3
= <normoslem256@gmail.com>
2024-12-21 12:36:42 +0300

    refactor(__init__.py) : made sure it returns all methods and classes from relateed files

--- minitales/__init__.py
+++ minitales/__init__.py
@@ -1,25 +1,3 @@
-import torch
-import torch.nn.functional as F
-
-def generate(model, tokenizer, device, prompt: str, max_length=50, temperature=1.0, top_k=10, eos_token='<|eos|>'):
-    model.eval()
-    enc = tokenizer.encode(prompt)
-
-    for _ in range(max_length):
-        input_tensor = torch.tensor(enc).unsqueeze(0).to(device)
-        output = model(input_tensor)  # Forward pass through the model
-
-        logits = output[:, -1, :].squeeze(0)
-
-        probabilities = F.softmax(logits, dim=-1)
-
-        next_token = torch.multinomial(probabilities, 1).item()
-
-        if tokenizer.vocab[next_token] == eos_token:
-          break
-
-        enc.append(next_token)
-
-    dec = tokenizer.decode(enc)
-
-    return dec
+from .model import *
+from .train import *
+from .tools import *
