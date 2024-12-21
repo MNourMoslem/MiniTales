cd582ffb66f3ea417fb0976e65dd025fdbb10c97
= <normoslem256@gmail.com>
2024-12-21 12:39:05 +0300

    refactor(train.py): deleted evaluate function and implemented the batching funcionality from tools.get_batch. also changed the losses variable in train fucntion to a list containes all avg losses

--- minitales/train.py
+++ minitales/train.py
@@ -1,47 +1,24 @@
 import torch
-from torch.optim.lr_scheduler import StepLR
+from .tools import generate, get_batch
 
-def evaluate(model, criterion, tokens, block_size, batch_size, n_samples, device):
-    """
-    Evaluate the model on the test dataset and calculate the loss.
-    
-    Parameters:
-    - model: The trained model to evaluate.
-    - criterion: The loss function used for evaluation.
-    - test_data: The dataset to evaluate the model on.
-    - block_size: The size of the input blocks.
-    - batch_size: The number of samples per batch.
-    - device: The device to run the evaluation on (CPU or GPU).
-    
-    Returns:
-    - avg_loss: The average loss over the test dataset.
-    """
-    model.eval()  # Set the model to evaluation mode
-
-    upper = len(tokens) - block_size - 1
-
-    total_loss = 0
-    with torch.no_grad():  # Disable gradient calculation
-        for _ in range(n_samples):
-            indices = torch.randint(0, upper, (batch_size,)).to(device)  # Generate all random indices at once
-            input_ = tokens[indices[:, None] + torch.arange(block_size, device=device)]  # Use broadcasting to create input
-            target = tokens[indices[:, None] + torch.arange(1, block_size + 1, device=device)]  # Use broadcasting to create target
-
-            output = model(input_).transpose(1, 2)  # Transpose for CrossEntropyLoss
-
-            loss = criterion(output[:, -1], target[:, -1])  # Compute loss
-            total_loss += loss
-
-    return total_loss / n_samples
-
-def train(model, optimizer, criterion, tokens, block_size, batch_size, device, epochs=20, sub_epochs=1000, n_samples=100, scheduler=None):
+def train(model : torch.nn.Module,
+          optimizer : torch.optim,
+          criterion,
+          tokens : torch.tensor,
+          block_size : int,
+          batch_size : int,
+          device : str,
+          epochs : int = 20,
+          sub_epochs : int = 1000,
+          n_samples : int = 100,
+          scheduler : torch.optim.lr_scheduler = None):
     """
     Train the model using the given optimizer and criterion.
     """
 
     upper = len(tokens) - block_size - 1
 
-    losses = {"train": [], "test": []}
+    losses = []
 
     print(f'Training for {epochs} epochs with {sub_epochs} sub-epochs each')
     for epoch in range(epochs):
@@ -49,9 +26,7 @@ def train(model, optimizer, criterion, tokens, block_size, batch_size, device, e
         total_loss = 0
 
         for _ in range(sub_epochs):  # Manually generating batches
-            indices = torch.randint(0, upper, (batch_size,)).to(device)  # Generate all random indices at once
-            input_ = tokens[indices[:, None] + torch.arange(block_size, device=device)]  # Use broadcasting to create input
-            target = tokens[indices[:, None] + torch.arange(1, block_size + 1, device=device)]  # Use broadcasting to create target
+            input_, target = get_batch(tokens, 0, upper, batch_size, block_size, device) # Use broadcasting to create target
 
             optimizer.zero_grad()  # Zero the parameter gradients
 
@@ -64,10 +39,7 @@ def train(model, optimizer, criterion, tokens, block_size, batch_size, device, e
             total_loss += loss.item()
 
         avg_loss = total_loss / sub_epochs  # Corrected averaging
-        losses["train"].append(avg_loss)
-
-        # test_loss = evaluate(model, criterion, tokens, block_size, batch_size, n_samples, device)
-        # losses["test"].append(test_loss)
+        losses.append(avg_loss)
 
         print(f'Epoch [{epoch+1}/{epochs}], Loss: {avg_loss:.4f}')
 
